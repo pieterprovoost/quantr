35d9e49f85bc52bfd74b9050fa2394b36568596d
Pieter Provoost <pieterprovoost@gmail.com>
2022-02-28 22:08:43 +0100

    import fix

--- R/quantr.R
+++ R/quantr.R
@@ -3,7 +3,7 @@
 #' @docType package
 #' @name quantr
 #' @import dplyr lubridate tibble jsonlite readr ggplot2
-#' @importFrom rvest read_html html_element html_elements html_text html_children
+#' @importFrom rvest read_html html_table html_element html_elements html_text html_children
 #' @importFrom glue glue
 #' @author Pieter Provoost, \email{pieterprovoost@gmail.com}
 NULL
